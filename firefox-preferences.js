786b7c33b1bb71d99d2a9952be4be6754982360d
Giorgio Sironi <info@giorgiosironi.com>
2016-04-03 11:26:40 +0200

    Some customization steps for Gnome and Firefox

---
+++ firefox-preferences.js
@@ -0,0 +1,4 @@
+// open Google searches in a new tab
+pref("browser.search.openintab", true);
+// resume previous browser session
+pref("browser.startup.page", 3);
--- install.sh
+++ install.sh
@@ -8,5 +8,17 @@ rm ~/.bashrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 rm ~/.inputrc
 ln -s ~/dotfiles/.inputrc ~/.inputrc
-sudo apt-get install tmux
+
+# tmux, Vim and such
+sudo apt-get install `cat ~/dotfiles/packages.txt`
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# set up Right Alt as the compose key for accented letters
+# set up Caps Lock as an additional Ctrl
+gsettings set org.gnome.desktop.input-sources xkb-options "['compose:ralt', 'ctrl:nocaps']"
+# gedit creates no backup files with ~ suffix
+gsettings set org.gnome.gedit.preferences.editor create-backup-copy false
+
+# Firefox preferences
+sudo cp firefox-preferences.js /usr/lib/firefox/browser/defaults/preferences/
+
